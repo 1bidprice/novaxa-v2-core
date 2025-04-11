d7cb445599123a5cf08f32a67a8d8c6691934d6f
1bidprice <bidprice.alerts@gmail.com>
2025-04-11 23:48:52 +0300

    Update enhanced_bot.py

--- enhanced_bot.py
+++ enhanced_bot.py
@@ -93,4 +93,5 @@ def set_webhook():
 
 # Entrypoint for gunicorn
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
